3ec379062084ac51e252e59088dce9e93d6db707
Ashish Dalvi <223427057+dalvi-ashish-git@users.noreply.github.com>
2026-02-16 05:24:31 +0530

    Uploaded Experiment 3 Project Folder

---
+++ Exp03 - Drawing Canvas/app/src/main/java/co/martinbaciga/drawingtest/MainActivity.kt
@@ -0,0 +1,47 @@
+package co.martinbaciga.drawingtest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import co.martinbaciga.drawingtest.ui.theme.Exp3DrawingCanvasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Exp3DrawingCanvasTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Exp3DrawingCanvasTheme {
+        Greeting("Android")
+    }
+}
